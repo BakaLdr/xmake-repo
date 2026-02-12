bcae7c97a3569f75dcfce4da3151edf53d8e9e8a
Singl <SingleIsland@outlook.com>
2026-02-13 02:44:12 +0800

    update package: bakaperms

--- packages/b/bakaperms/xmake.lua
+++ packages/b/bakaperms/xmake.lua
@@ -3,7 +3,7 @@ package("bakaperms")
 
     add_urls("https://github.com/BakaLdr/BakaPerms.git")
     add_versions("0.1.0", "fea77cf565b5814a8ac475e43b6310e4b2f051ba")
-    add_versions("0.1.1", "9bc3ffa230a47d32aebb2ec73a4f3eead7cf5e9d")
+    add_versions("0.1.1", "c7a77643aeee9e42269dc1387df5be3fcaa8ad5d")
 
     on_install(function (package)
         local configs = {}
