a295150b09c47ce5ce4f8c60bdd6b8f0c446bcf4
Singl <SingleIsland@outlook.com>
2026-02-12 16:26:00 +0800

    add package: bakaperms

---
+++ README.md
@@ -0,0 +1,3 @@
+```
+add_repositories("bakaldr-repo https://github.com/BakaLdr/xmake-repo.git")
+```
---
+++ packages/b/bakaperms/xmake.lua
@@ -0,0 +1,10 @@
+package("bakaperms")
+    set_description("A library for managing permissions for LeviLamina")
+
+    add_urls("https://github.com/BakaLdr/BakaPerms")
+    add_versions("0.1.0", "fea77cf565b5814a8ac475e43b6310e4b2f051ba")
+
+    on_install(function (package)
+        local configs = {}
+        import("package.tools.xmake").install(package, configs)
+    end)
